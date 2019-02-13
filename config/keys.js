ca12fb8e4b53ee978440890c3a8e7faf74191edd
Nicholas Mahase <nicholasmahase@gmail.com>
2019-02-13 18:32:53 -0500

    updates

--- config/keys.js
+++
@@ -1,7 +0,0 @@
-console.log('this is loaded');
-
-exports.api = {
-  id: process.env.NEWS_API,
-  id: process.env.NYT_API,
-  
-};
--- server.js
+++ server.js
@@ -1,116 +1,60 @@
-var express = require("express");
-var login = require("./routes/loginroutes");
-var bodyParser = require("body-parser");
+var express = require('express');
 var app = express();
+var passport = require('passport');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var env = require('dotenv').load();
+var exphbs = require('express-handlebars');
+
+
+//For BodyParser
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-var router = express.Router();
-// test route
-router.get("/", function(req, res) {
-  res.json({ message: "welcome to our upload module apis" });
-});
-//route to handle user registration
-router.post("/register", login.register);
-router.post("/login", login.login);
-app.use("/api", router);
-app.listen(8080);
 
-// Create My SQL connection
+// For Passport
+ 
+app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true})); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
 
-var mysql = require("mysql");
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "your-password",
-  database: "cloudprint"
-});
-connection.connect(function(err) {
-  if (!err) {
-    console.log("Database is connected ... nn");
-  } else {
-    console.log("Error connecting database ... nn");
-  }
+//For Handlebars
+app.set('views', './app/views')
+app.engine('hbs', exphbs({
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+ 
+app.get('/', function(req, res) {
+ 
+    res.send('Welcome to Passport with Sequelize');
+ 
 });
 
-// Create Handler for User Registration
+//Models
+var models = require("./app/models");
 
-exports.register = function(req, res) {
-  // console.log("req",req.body);
-  var today = new Date();
-  var users = {
-    first_name: req.body.first_name,
-    last_name: req.body.last_name,
-    email: req.body.email,
-    password: req.body.password,
-    created: today,
-    modified: today
-  };
-  connection.query("INSERT INTO users SET ?", users, function(
-    error,
-    results,
-    fields
-  ) {
-    if (error) {
-      console.log("error ocurred", error);
-      res.send({
-        code: 400,
-        failed: "error ocurred"
-      });
-    } else {
-      console.log("The solution is: ", results);
-      res.send({
-        code: 200,
-        success: "user registered sucessfully"
-      });
-    }
-  });
-};
+//Routes
+var authRoute = require('./app/routes/auth.js')(app,passport); 
 
-// Insertion query for Users added into the database
+//load passport strategies
+require('./app/config/passport/passport.js')(passport, models.user);
 
-exports.login = function(req, res) {
-  var email = req.body.email;
-  var password = req.body.password;
-  connection.query("SELECT * FROM users WHERE email = ?", [email], function(
-    error,
-    results,
-    fields
-  ) {
-    if (error) {
-      // console.log("error ocurred",error);
-      res.send({
-        code: 400,
-        failed: "error ocurred"
-      });
-    } else {
-      // console.log('The solution is: ', results);
-      if (results.length > 0) {
-        if (results[0].password == password) {
-          res.send({
-            code: 200,
-            success: "login sucessfull"
-          });
-        } else {
-          res.send({
-            code: 204,
-            success: "Email and password does not match"
-          });
-        }
-      } else {
-        res.send({
-          code: 204,
-          success: "Email does not exits"
-        });
-      }
-    }
-  });
-};
+//Sync Database
+models.sequelize.sync().then(function() {
+ 
+    console.log('Nice! Database looks fine')
+ 
+}).catch(function(err) {
+ 
+    console.log(err, "Something went wrong with the Database Update!")
+ 
+});
+ 
+app.listen(5000, function(err) {
+ 
+    if (!err)
+        console.log("Site is live");
+    else console.log(err)
+ 
+});
 
